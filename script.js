c32a6edde62331ccbc63bbf16a276315b17a2ea3
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-04 13:29:42 -0700

    Added Functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let turn = "x";
 let turnaudio = new Audio("ting.mp3");
 let myturntext = document.querySelector(".info");
 const myboxes = document.querySelectorAll(".box");
+const win = document.querySelector(".win");
+let reset = document.querySelector("#reset");
 console.log(myboxes[2]);
 function changeturn() {
   if (turn == "x") {
@@ -12,7 +14,6 @@ function changeturn() {
 }
 function checkwin() {
   if (
-    // Horizontal rows
     (myboxes[0].innerHTML !== "" &&
       myboxes[0].innerHTML === myboxes[1].innerHTML &&
       myboxes[1].innerHTML === myboxes[2].innerHTML) ||
@@ -22,7 +23,6 @@ function checkwin() {
     (myboxes[6].innerHTML !== "" &&
       myboxes[6].innerHTML === myboxes[7].innerHTML &&
       myboxes[7].innerHTML === myboxes[8].innerHTML) ||
-    // Vertical columns
     (myboxes[0].innerHTML !== "" &&
       myboxes[0].innerHTML === myboxes[3].innerHTML &&
       myboxes[3].innerHTML === myboxes[6].innerHTML) ||
@@ -32,15 +32,16 @@ function checkwin() {
     (myboxes[2].innerHTML !== "" &&
       myboxes[2].innerHTML === myboxes[5].innerHTML &&
       myboxes[5].innerHTML === myboxes[8].innerHTML) ||
-    // Diagonals
     (myboxes[0].innerHTML !== "" &&
-      myboxes[0].innerHTML === myboxes[5].innerHTML &&
-      myboxes[5].innerHTML === myboxes[9].innerHTML) ||
-    (myboxes[3].innerHTML !== "" &&
-      myboxes[3].innerHTML === myboxes[5].innerHTML &&
-      myboxes[5].innerHTML === myboxes[7].innerHTML)
+      myboxes[0].innerHTML === myboxes[4].innerHTML &&
+      myboxes[4].innerHTML === myboxes[8].innerHTML) ||
+    (myboxes[2].innerHTML !== "" &&
+      myboxes[2].innerHTML === myboxes[4].innerHTML &&
+      myboxes[4].innerHTML === myboxes[6].innerHTML)
   ) {
     return true;
+  } else {
+    return false;
   }
 }
 
@@ -48,12 +49,41 @@ myboxes.forEach(function (box) {
   box.addEventListener("click", function () {
     box.innerHTML = turn;
     turnaudio.play();
-    changeturn(); // change
-    myturntext.innerHTML = "Turn for " + turn;
     box.style.pointerEvents = "none";
-    checkwin();
     if (checkwin()) {
-      console.log("win");
+      setTimeout(resetgame, 1500);
+      win.style.display = "block";
+      win.innerHTML = turn + " wins";
+      myboxes.forEach(function (box) {
+        box.style.pointerEvents = "none";
+      });
+    } else if (checkDraw()) {
+      setTimeout(resetgame, 1500);
+      win.style.display = "block";
+      win.innerHTML = "Game Draw";
+    } else {
+      changeturn();
+      myturntext.innerHTML = "Turn for " + turn;
     }
+    z;
   });
 });
+function checkDraw() {
+  for (let box of myboxes) {
+    if (box.innerHTML === "") {
+      return false; 
+    }
+  }
+  return true; 
+}
+function resetgame() {
+  myboxes.forEach(function (box) {
+    box.innerHTML = "";
+    box.style.pointerEvents = "auto";
+  });
+  turn = "x";
+  myturntext.innerHTML = "Turn for " + turn;
+  win.style.display = "none";
+}
+
+reset.addEventListener("click", resetgame);
